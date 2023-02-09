6a9dfcbfc254a7269e9f5f34bc8400fdbdf1c13f
Schekhovtsov <schekhovtsov@gmail.com>
2023-02-09 20:23:45 +0300

    feat: module 2, lesson 11

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import { Suspense } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -12,12 +10,7 @@ const App = () => {
             <button onClick={toggleTheme}>Change theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            {Object.values(routeConfig).map(({ path, element }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={
+                        <Suspense fallback={<div>Loading...</div>}>
+                            {element}
+                        </Suspense>
+                    }
+                />
+            ))}
+        </Routes>
+    );
+};
