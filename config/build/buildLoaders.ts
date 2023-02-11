809bc8e917d080375220cdca0438a15721a0ade4
Schekhovtsov <schekhovtsov@gmail.com>
2023-02-11 18:36:10 +0300

    feat: module 2, lesson 17

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,17 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    };
+
     const urlLoader = {
         test: /\.png/,
         type: 'asset/resource',
@@ -40,5 +51,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
-    return [typeScriptLoader, cssLoader, svgLoader, urlLoader];
+    return [babelLoader, typeScriptLoader, cssLoader, svgLoader, urlLoader];
 }
