a5c320ce749137420e1864858bd15e1ad872fd91
Schekhovtsov <schekhovtsov@gmail.com>
2023-02-18 12:26:21 +0300

    feat: analyze script in package.json

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -12,11 +12,13 @@ export type BuildPaths = {
 export type BuildEnv = {
     mode: BuildMode;
     port: number;
+    analyze: boolean;
 };
 
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
     isDev: boolean;
+    analyze: boolean;
     port: number;
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -13,6 +13,7 @@ export default (env: BuildEnv) => {
 
     const mode = env.mode || 'development';
     const isDev = mode === 'development';
+    const { analyze } = env;
     const PORT = env.port || 3000;
 
     const config: webpack.Configuration = buildWebpackConfig({
@@ -20,6 +21,7 @@ export default (env: BuildEnv) => {
         paths,
         isDev,
         port: PORT,
+        analyze,
     });
 
     return config;
