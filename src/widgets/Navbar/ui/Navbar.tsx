f8b51790c700d816a291e9693945bf4ed00e636e
Schekhovtsov <schekhovtsov@gmail.com>
2023-02-20 21:22:04 +0300

    feat: module 2, lesson 28 - Sidebar update

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames';
-import { AppLink } from 'shared/ui/AppLink/ui/AppLink';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -12,10 +11,7 @@ export function Navbar({ className }: NavbarProps) {
 
     return (
         <div className={classNames(styles.container, {}, [className])}>
-            <div className={styles.linksContainer}>
-                <AppLink to="/">{t('Home')}</AppLink>
-                <AppLink to="/about">{t('About')}</AppLink>
-            </div>
+            {t('Site name')}
         </div>
     );
 }
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink } from 'shared/ui/AppLink';
+import HomeIcon from 'shared/assets/icons/home.svg';
+import AboutIcon from 'shared/assets/icons/about.svg';
 import { LanguageSwitcher } from 'widgets/LanguageSwitcher';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { ToggleSidebar } from 'widgets/ToggleSidebar';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import styles from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +15,7 @@ interface SidebarProps {
 }
 
 export function Sidebar({ className }: SidebarProps) {
+    const { t } = useTranslation();
     const [isCollapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
@@ -25,6 +31,18 @@ export function Sidebar({ className }: SidebarProps) {
             )}
             data-testid="sidebar"
         >
+            <div className={styles.links}>
+                <AppLink to={RoutePath.main} className={styles.item}>
+                    <HomeIcon className={styles.icon} />
+                    <span className={styles.link}>{t('Home')}</span>
+                </AppLink>
+
+                <AppLink to={RoutePath.about} className={styles.item}>
+                    <AboutIcon className={styles.icon} />
+                    <span className={styles.link}>{t('About')}</span>
+                </AppLink>
+            </div>
+
             <div className={styles.switchers}>
                 <ToggleSidebar
                     isCollapsed={isCollapsed}
