860f3114f46e5789594bbaa286c5652e669908fd
Schekhovtsov <schekhovtsov@gmail.com>
2023-04-23 22:53:48 +0300

    feat: lesson 56 / fixes /

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.tsx
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.tsx
@@ -14,10 +14,12 @@ export const useInfiniteScroll = ({
     // eslint-disable-next-line consistent-return
     useEffect(() => {
         let observer: IntersectionObserver | null = null;
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
 
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '40px',
                 threshold: 1.0,
             };
@@ -28,12 +30,12 @@ export const useInfiniteScroll = ({
                 }
             }, options);
 
-            observer.observe(triggerRef.current);
+            observer.observe(triggerElement);
 
             return () => {
                 if (observer) {
                     // eslint-disable-next-line react-hooks/exhaustive-deps
-                    observer?.unobserve(triggerRef.current);
+                    observer?.unobserve(triggerElement);
                 }
             };
         }
